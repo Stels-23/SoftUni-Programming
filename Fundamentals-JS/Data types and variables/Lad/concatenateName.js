fdd451ae53d38eec7cd5c3ac918741d3094c5d04
Stels-23 <103596296+Stels-23@users.noreply.github.com>
2022-04-15 13:09:49 +0300

    Delete Fundamentals-JS/Data types and variables/Lad directory

--- Fundamentals-JS/Data types and variables/Lad/concatenateName.js
+++
@@ -1,9 +0,0 @@
-/*function concatenate(name, lN, delimiter) {
-    console.log(`${name}${delimiter}${lN}`)
-}*/
-function concatenate(name, lN, delimiter) {
-    console.log(name + delimiter + lN);
-}
-concatenate('Linda', 'Terry', '=>')
-    //(')Jan', 'White', '<->')
-    //('John', 'Smith', '->')
--- Fundamentals-JS/Data types and variables/Lad/requiredReading.js
+++
@@ -1,6 +0,0 @@
-function requiredReading(countPage, pagesReadingFor1H, countDays) {
-    let totalTimeToReadTheBook = countPage / pagesReadingFor1H;
-    let requaredPerH = totalTimeToReadTheBook / countDays;
-    console.log(requaredPerH);
-}
-requiredReading(212, 20, 2); //(432,15 ,4)
