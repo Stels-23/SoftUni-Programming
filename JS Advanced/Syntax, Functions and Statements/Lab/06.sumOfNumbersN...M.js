41287ba17f731fd498dd212b52b89954aaae8f91
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-04-30 10:04:27 +0300

    Add files via upload

---
+++ JS Advanced/Syntax, Functions and Statements/Lab/06.sumOfNumbersN...M.js
@@ -0,0 +1,12 @@
+function sumOfNumbers(n1, n2) {
+    let num1 = Number(n1);
+    let num2 = Number(n2);
+    let result = 0;
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+    }
+    console.log(result);
+}
+sumOfNumbers('1', '5')
+console.log('=====================');
+sumOfNumbers('-8', '20');
