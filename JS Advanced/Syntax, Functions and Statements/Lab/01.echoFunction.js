db15b532304cb3f0389691febdf35ad3ee1677e0
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-04-30 09:59:18 +0300

    Add files via upload

---
+++ JS Advanced/Syntax, Functions and Statements/Lab/01.echoFunction.js
@@ -0,0 +1,10 @@
+function echoFunction(input) {
+    let textL = input.length;
+    let text = input;
+    console.log(`${textL}\n${text}`);
+
+
+}
+echoFunction('Hello, JavaScript!');
+console.log('============')
+echoFunction('strings are easy');
