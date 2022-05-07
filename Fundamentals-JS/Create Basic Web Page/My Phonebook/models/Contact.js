a51e417aad03765865a909d05a5ab451c49acf13
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-05-07 18:29:24 +0300

    Add files via upload

---
+++ Fundamentals-JS/Create Basic Web Page/My Phonebook/models/Contact.js
@@ -0,0 +1,9 @@
+// TODO: Create Contact model and export it
+class Contact {
+    constructor(name, number) {
+        this.name = name;
+        this.number = number;
+    }
+}
+//правим публичен каласа
+module.exports = Contact;
