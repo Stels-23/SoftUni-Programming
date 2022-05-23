9e9996a799961dd06247d8701f8396293cd054af
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-05-23 10:18:47 +0300

    Update 08.validityChecker-1.js

--- JS Advanced/Syntax, Functions and Statements/Ex/08.validityChecker-1.js
+++ JS Advanced/Syntax, Functions and Statements/Ex/08.validityChecker-1.js
@@ -7,19 +7,7 @@ function solve(n1, n2, n3, n4) {
     result(x1, y1, 0, 0);
     result(x2, y2, 0, 0);
     result(x1, y1, x2, y2);
-    /*console.log(Number.isInteger(result(x1, y1)) ?
-        `{${x1}, ${y1}} to {0, 0} is valid` :
-        `{${x1}, ${y1}} to {0, 0} is invalid`);
-    console.log(Number.isInteger(result(x2, y2)) ?
-        `{${x2}, ${y2}} to {0, 0} is valid` :
-        `{${x2}, ${y2}} to {0, 0} is invalid`);
-
-    console.log(Number.isInteger(result(x1 - x2), result(y1 - y2)) ?
-        `{${x1}, ${y1}} to {${x2}, ${y2}} is valid` :
-        `{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);*/
-
-
-    function result(x1, y1, x2, y2) {
+function result(x1, y1, x2, y2) {
         let distance = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
         let validString = Number.isInteger(distance) ? 'valid' : 'invalid';
         console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${validString}`)
