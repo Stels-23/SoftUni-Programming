3828edadec6b36d505e89adbebb2a296b54ca6f7
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-05-27 09:50:51 +0300

    Add files via upload

---
+++ JS Advanced/Object and composition/01.cityRecord.js
@@ -0,0 +1,14 @@
+function cityRecord(name, population, treasury) {
+    const obj = {
+        name,
+        population,
+        treasury
+    }
+    return obj
+}
+cityRecord('Tortuga',
+    7000,
+    15000);
+cityRecord('Santo Domingo',
+    12000,
+    23500)
