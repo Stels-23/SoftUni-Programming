ed6ad811883a7675297dd5f4da2f8c0b51205b27
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-05-29 18:16:07 +0300

    Add files via upload

---
+++ JS Advanced/Arrays and Nested Arrays/Exercises/05.exWithReduce.js
@@ -0,0 +1,37 @@
+function extractBigN(array) {
+    let newArr = [];
+    let biggestN = array[0];
+    newArr.push(array[0]);
+    array.reduce((num1, num2, index, arr) => {
+        if (biggestN <= num2) {
+            newArr.push(num2);
+            biggestN = num2;
+        }
+
+    });
+
+
+    console.log(newArr)
+}
+extractBigN([1,
+    3,
+    8,
+    4,
+    10,
+    12,
+    3,
+    2,
+    24
+]);
+extractBigN([1,
+    2,
+    3,
+    4
+]);
+extractBigN([20,
+    3,
+    2,
+    15,
+    6,
+    1
+]);
