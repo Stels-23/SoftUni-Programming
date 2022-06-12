c8a922f876cb1d1934fdeacc8fc9c948cadfd6ff
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-06-12 15:21:31 +0300

    Add files via upload

---
+++ JS Advanced/Object and composition/05.asseblyLine.js
@@ -0,0 +1,52 @@
+function assemblyLine() {
+
+    return {
+        hasClima: (myCar) => {
+            myCar.temp = 21;
+            myCar.tempSettings = 21;
+            myCar.adjustTemp = () => {
+                if (myCar.temp < myCar.tempSettings) {
+                    myCar.temp++;
+                } else if (myCar.temp > myCar.tempSettings) {
+                    myCar.temp--;
+                }
+
+            };
+        },
+        hasAudio: (myCar) => {
+            myCar.currentTrack = { 'name': '', 'artist': '' };
+            myCar.nowPlaying = () => {
+                if (myCar.currentTrack !== null) {
+                    console.log(`Now playing '${myCar.currentTrack.name}' by ${myCar.currentTrack.artist}`);
+                }
+            };
+        },
+        hasParktronic: (myCar) => {
+            myCar.checkDistance = (distance) => {
+                if (distance < 0.1) {
+                    console.log('Beep! Beep! Beep!');
+                } else if (distance >= 0.1 && distance < 0.25) {
+                    console.log('Beep! Beep!');
+                } else if (distance >= 0.25 && distance < 0.5) {
+                    console.log('Beep!');
+                } else {
+                    console.log('');
+                }
+            };
+        }
+    };
+}
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+console.log(myCar);
