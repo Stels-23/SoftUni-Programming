112dd1d369dceecde861e1600f343607b773d834
Steliyana Petkova <103596296+Stels-23@users.noreply.github.com>
2022-06-18 13:24:37 +0300

    Update 02.constructionScrew.js

--- JS Advanced/Object and composition/Exercises/02.constructionScrew.js
+++ JS Advanced/Object and composition/Exercises/02.constructionScrew.js
@@ -1,34 +1,3 @@
-/*function constructionCrew(obj) {
-    let newObj = {};
-    const requareAmountPerKG = 0.1;
-    for (let [key, value] of Object.entries(obj)) {
-        if (key === 'levelOfHydrated') {
-            let level = 0;
-            for (let [el1, el2] of Object.entries(obj)) {
-
-                if (el1 === 'weight') {
-                    level = el2 * requareAmountPerKG;
-                } else if (el1 === 'experience') {
-                    level *= el2;
-                } else if (el1 === 'levelOfHydrated') {
-                    level += el2;
-                    if (Number.isInteger(level) === false) {
-                        level = 0;
-                    }
-                }
-                newObj[key] = level;
-            }
-
-        } else if (key === 'dizziness') {
-            newObj[key] = false;
-        } else {
-            newObj[key] = value;
-        }
-
-    }
-    return newObj
-
-}*/
 function constructionCrew(worker) {
     const requareAmountPerKG = 0.1;
     if (worker.dizziness) { // if human is hydrated
